9612116995684c3acd07a1ddb96486b0327ae045
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-03 19:25:24 +0530

    removed an error

--- apna-cart/src/components/ProductList.js
+++ apna-cart/src/components/ProductList.js
@@ -3,8 +3,12 @@ import Product from "./Product.js";
 
 export default function ProductList(props) {
   return (
-    props.products.map((product) => {
-        return <Product product = {product}/>
+    props.products.map((product,idx) => {
+        return <Product product = {product} key = {idx}/>
     })
   );
 }
+// we are getting the warning in console:
+// Warning: Each child in a list should have a unique "key" prop.
+// To remove this error we need to introduce a unique key as a part of the props passing so that react can understand
+// that one component is different from another one.
