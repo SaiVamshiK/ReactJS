806191113d42e6dc9511a7b3951acad9e527b3a0
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-03 18:46:39 +0530

    other components for cart created

--- apna-cart/src/App.js
+++ apna-cart/src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar.js";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
       <>
         <Navbar/>
+        <ProductList/>
+        <Footer/>
       </>
   );
 }
