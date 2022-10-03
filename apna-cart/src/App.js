ffbd5750bccbb0841a9b4f0c06fc165d8a87821d
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-03 18:57:46 +0530

    passing props

--- apna-cart/src/App.js
+++ apna-cart/src/App.js
@@ -5,10 +5,22 @@ import ProductList from "./components/ProductList";
 import Footer from "./components/Footer";
 
 function App() {
+  const products = [
+    {
+      price : 10999,
+      model : 'IPhone 14',
+      quantity : 3
+    },
+    {
+      price : 28374,
+      model : 'IPhone 14 max',
+      quantity : 2
+    }
+  ]
   return (
       <>
         <Navbar/>
-        <ProductList/>
+        <ProductList products = {products}/>
         <Footer/>
       </>
   );
--- apna-cart/src/components/ProductList.js
+++ apna-cart/src/components/ProductList.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function ProductList() {
+export default function ProductList(props) {
+  console.log(props);
   return (
     <div>
-      
+
     </div>
-  )
+  );
 }
