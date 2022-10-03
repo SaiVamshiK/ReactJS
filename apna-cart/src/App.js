020dab10d75aa40fb380ab33b3dc5c7a3fb16f1d
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-03 18:40:49 +0530

    navbar for apna cart

--- apna-cart/src/App.js
+++ apna-cart/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
-    <>
-      
-    </>
+      <>
+        <Navbar/>
+      </>
   );
 }
 
