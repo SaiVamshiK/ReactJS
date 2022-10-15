f8ac2930eecb8a4ee5aef79ccbdc353cdfdef091
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-15 12:10:02 +0530

    resolved the error

--- cart-application/src/components/ProductList.js
+++ cart-application/src/components/ProductList.js
@@ -4,8 +4,8 @@ import Product from "./Product.js";
 export default function ProductList(props) {
   console.log(props);
   return (
-    props.products.map((product) => {
-        return <Product product = {product}/>
+    props.products.map((product,index) => {
+        return <Product product = {product} key = {index}/>
     })
   );
 }
