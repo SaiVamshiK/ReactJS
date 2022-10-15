1d9ccfda2aaea1c948bf1811d5c5fa218edd9589
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-15 11:42:53 +0530

    Components added

--- cart-application/src/App.js
+++ cart-application/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar.js";
+import ProductList from "./components/ProductList.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
