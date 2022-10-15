0c3f814360ae33d2b4beabbb1450c20b23458a23
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-15 11:45:46 +0530

    dummy product list creation

--- cart-application/src/App.js
+++ cart-application/src/App.js
@@ -5,6 +5,23 @@ import ProductList from "./components/ProductList.js";
 import Footer from "./components/Footer.js";
 
 function App() {
+  const productList = [
+    {
+      price : 43839,
+      name : "IPhone 14",
+      quantity : 2
+    },
+    {
+      price : 43787,
+      name : "IPhone 9",
+      quantity : 4
+    },
+    {
+      price : 81447,
+      name : "IPhone 13",
+      quantity : 1
+    }
+  ]
   return (
     <>
       <Navbar />
