bc5af1a606930ebf10419e509f913015b3470b05
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-15 17:56:23 +0530

    minor changes

--- cart-application/src/components/ProductList.js
+++ cart-application/src/components/ProductList.js
@@ -2,9 +2,16 @@ import React from "react";
 import Product from "./Product.js";
 
 export default function ProductList(props) {
-  return (
-    props.products.map((product,index) => {
-        return <Product product = {product} key = {index} incrementQuantity = {props.incrementQuantity} decrementQuantity = {props.decrementQuantity} index = {index} removeProduct = {props.removeProduct}/>
-    })
-  );
+    if(props.products.length > 0){
+        return (
+            props.products.map((product,index) => {
+                return <Product product = {product} key = {index} incrementQuantity = {props.incrementQuantity} decrementQuantity = {props.decrementQuantity} index = {index} removeProduct = {props.removeProduct}/>
+            })
+          );
+    }else{
+        return (
+            <h3>No Product to display</h3>
+        );
+    }
+  
 }
