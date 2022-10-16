b729e7fd6c603ec509831c7fbdf6840ffc5dc085
SaiVamshiK <saivamshi.k.24@gmail.com>
2022-10-16 12:13:42 +0530

    displaying count of product Items available

--- cart-application/src/App.js
+++ cart-application/src/App.js
@@ -3,25 +3,14 @@ import "./App.css";
 import Navbar from "./components/Navbar.js";
 import ProductList from "./components/ProductList.js";
 import Footer from "./components/Footer.js";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import AddItem from "./components/AddItem.js";
-
 
 function App() {
   const products = [
     {
       price: 2000,
       name: "IPhone 14",
-      quantity: 0,
-    },
-    {
-      price: 1600,
-      name: "IPhone 9",
-      quantity: 0,
-    },
-    {
-      price: 2300,
-      name: "IPhone 13",
       quantity: 0,
     },
     {
@@ -39,76 +28,94 @@ function App() {
       name: "Samsung 13",
       quantity: 0,
     },
-  ]
-  let [productList,setProductList] = useState(products);
-  let [totalAmount,setTotalAmount] = useState(0);
-
+  ];
+  let [productList, setProductList] = useState(products);
+  let [totalAmount, setTotalAmount] = useState(0);
+  let [productCount, setProductCount] = useState(products.length);
 
   const incrementQuantity = (index) => {
-    let newProductList = [...productList]
-    newProductList[index].quantity++
-    setProductList(newProductList)
+    let newProductList = [...productList];
+    newProductList[index].quantity++;
+    setProductList(newProductList);
     // totalAmount += newProductList[index].price
     // setTotalAmount(totalAmount)
-    let curTotalAmount = totalAmount
-    curTotalAmount += newProductList[index].price
-    setTotalAmount(curTotalAmount)
-  }
+    let curTotalAmount = totalAmount;
+    curTotalAmount += newProductList[index].price;
+    setTotalAmount(curTotalAmount);
+
+  };
   const decrementQuantity = (index) => {
-    let newProductList = [...productList]
-    if(newProductList[index].quantity !== 0){
+    let newProductList = [...productList];
+    if (newProductList[index].quantity !== 0) {
       newProductList[index].quantity--;
-      setProductList(newProductList)
+      setProductList(newProductList);
       // totalAmount -= newProductList[index].price
       // setTotalAmount(totalAmount)
-      let curTotalAmount = totalAmount
-      curTotalAmount -= newProductList[index].price
-      setTotalAmount(curTotalAmount)
+      let curTotalAmount = totalAmount;
+      curTotalAmount -= newProductList[index].price;
+      setTotalAmount(curTotalAmount);
+
     }
-  }
+  };
 
   const resetButton = () => {
-    let newProductList = [...productList]
+    let newProductList = [...productList];
     newProductList = newProductList.map((product) => {
-      product.quantity = 0
-      return product
-    })
-    setProductList(newProductList)
-    let newTotalAmount = 0
-    setTotalAmount(newTotalAmount)
-  }
+      product.quantity = 0;
+      return product;
+    });
+    setProductList(newProductList);
+    let newTotalAmount = 0;
+    setTotalAmount(newTotalAmount);
 
-  const findIndexAmount = (productList,index) => {
-    return productList[index].quantity*productList[index].price
-  }
+  };
+
+  const findIndexAmount = (productList, index) => {
+    return productList[index].quantity * productList[index].price;
+  };
 
   const removeProduct = (index) => {
-    let curTotalAmount = totalAmount
-    curTotalAmount -= findIndexAmount(productList,index)
-    let newProductList = [...productList]
-    newProductList = newProductList.filter((elem) => elem !== productList[index])
-    setProductList(newProductList)
-    setTotalAmount(curTotalAmount)
-  }
-  
-  const addProduct = (name,price) => {
-    let newProductList = [...productList]
+    let curTotalAmount = totalAmount;
+    curTotalAmount -= findIndexAmount(productList, index);
+    let newProductList = [...productList];
+    newProductList = newProductList.filter(
+      (elem) => elem !== productList[index]
+    );
+    setProductList(newProductList);
+    setTotalAmount(curTotalAmount);
+
+    let newProductCount = productCount
+    newProductCount--;
+    setProductCount(newProductCount)
+  };
+
+  const addProduct = (name, price) => {
+    let newProductList = [...productList];
     newProductList.push({
-      price : price,
-      name : name,
-      quantity : 0
-    })
-    setProductList(newProductList)
-  }
+      price: price,
+      name: name,
+      quantity: 0,
+    });
+    setProductList(newProductList);
+
+    let newProductCount = productCount
+    newProductCount++;
+    setProductCount(newProductCount)
+  };
 
   return (
     <>
-      <Navbar />
+      <Navbar productCount = {productCount}/>
       <main className="container mt-5">
-        <AddItem addProduct = {addProduct}/>
-        <ProductList products={productList} incrementQuantity = {incrementQuantity} decrementQuantity ={decrementQuantity} removeProduct = {removeProduct}/>
+        <AddItem addProduct={addProduct} />
+        <ProductList
+          products={productList}
+          incrementQuantity={incrementQuantity}
+          decrementQuantity={decrementQuantity}
+          removeProduct={removeProduct}
+        />
       </main>
-      <Footer totalAmount = {totalAmount} resetButton = {resetButton}/>
+      <Footer totalAmount={totalAmount} resetButton={resetButton} />
     </>
   );
 }
--- cart-application/src/components/Navbar.js
+++ cart-application/src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 class Navbar extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid col-2">
           <a className="navbar-brand" href="#">
             Apna Cart
           </a>
+        </div>
+        <div className='col-4'>
+          Items : <span>{this.props.productCount}</span>
         </div>
       </nav>
     );
